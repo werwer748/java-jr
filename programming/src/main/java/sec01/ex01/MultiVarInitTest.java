4240ce9e1d9e9dc7211b53a6c3737f7f606b0e2a
hugo <werwer748@naver.com>
2024-03-01 11:12:20 +0900

    변수선언부터다시

---
+++ programming/src/main/java/sec01/ex01/MultiVarInitTest.java
@@ -0,0 +1,18 @@
+package sec01.ex01;
+
+public class MultiVarInitTest {
+    public static void main(String[] args) {
+
+        int num1, num2, num3; // 한줄에 동시 선언 하기
+
+        num1 = 1;
+        num2 = 2;
+        num3 = 3;
+
+//        int num1 = 1, num2 = 2, num3 = 3; // 한줄에 동시 선언과 동시에 초기화
+
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+        System.out.println("num3 = " + num3);
+    }
+}
